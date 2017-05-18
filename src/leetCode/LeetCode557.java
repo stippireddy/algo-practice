3e59fff3d6164938d6d23fab6b19eea5f92d173f
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-05-18 22:19:27 +0530

    Added a few more solutions to LeetCode problems

---
+++ src/leetCode/LeetCode557.java
@@ -0,0 +1,59 @@
+package leetCode;
+
+import java.util.StringTokenizer;
+
+/**
+ * Created by Sarveswara Tippireddy on 5/17/2017.
+ */
+public class LeetCode557 {
+    public String reverseWords(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        StringTokenizer tokenizer = new StringTokenizer(s);
+        StringBuffer sb = new StringBuffer();
+        while (tokenizer.hasMoreTokens()) {
+            String curr = tokenizer.nextToken();
+            for (int i = curr.length() - 1; i >= 0; i--) {
+                sb.append(curr.charAt(i));
+            }
+            sb.append(' ');
+        }
+        sb.deleteCharAt(s.length());
+        return sb.toString();
+    }
+
+    public String reverseWordsEfficiently(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        char[] input = s.toCharArray();
+        int i = 0;
+        for(int k = 0; k<input.length; k++){
+            if(input[k]==' '){
+                reverse(input, i,k-1);
+                i = k+1;
+            }
+        }
+        if (i < input.length) {
+            reverse(input, i, input.length - 1);
+        }
+
+        return String.valueOf(input);
+    }
+
+    private void reverse(char[] input, int i, int j){
+        while(i<j){
+            char temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public static void main(String[] args) {
+        LeetCode557 l = new LeetCode557();
+        System.out.println(l.reverseWordsEfficiently("Let's take LeetCode contest"));
+        ;
+    }
+}
---
+++ src/leetCode/LeetCode561.java
@@ -0,0 +1,39 @@
+package leetCode;
+
+import java.util.Arrays;
+
+/**
+ * Created by Sarveswara Tippireddy on 5/17/2017.
+ */
+public class LeetCode561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+        for (int i = 0; i < nums.length; i = i + 2) {
+            maxSum = maxSum + nums[i];
+        }
+        return maxSum;
+    }
+
+    public int arrayPairsSumEfficient(int[] nums) {
+        int[] hash = new int[20001];
+        boolean flag = true;
+        for (int i : nums) {
+            hash[i + 10000]++;
+        }
+        int sum = 0;
+        for (int i = 0; i < hash.length; ) {
+            if (hash[i] > 0 && flag == true) {
+                sum = sum + i - 10000;
+                flag = false;
+                hash[i]--;
+            } else if (hash[i] > 0 && flag == false) {
+                flag = true;
+                hash[i]--;
+            } else {
+                i++;
+            }
+        }
+        return sum;
+    }
+}
