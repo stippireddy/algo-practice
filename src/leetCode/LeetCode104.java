e74995e2bf4ff6cd85ebace7bc961bc0ba0a13ea
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-06-02 06:04:54 +0530

    Added a new solution using iterative DFS for LeetCode #104.

--- src/leetCode/LeetCode104.java
+++ src/leetCode/LeetCode104.java
@@ -1,5 +1,6 @@
 package leetCode;
 
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 
 import karumanchi.trees.TreeNode;
@@ -11,6 +12,31 @@ public class LeetCode104 {
       return 0;
     }
     return 1 + Integer.max(maxDepth(root.left), maxDepth(root.right));
+  }
+
+  public int maxDepthIterativeDFS(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    ArrayDeque<TreeNode> treeStack = new ArrayDeque<>();
+    ArrayDeque<Integer> depthStack = new ArrayDeque<>();
+    treeStack.push(root);
+    depthStack.push(1);
+    int max = 0;
+    while (!treeStack.isEmpty()) {
+      TreeNode tempNode = treeStack.pop();
+      int tempDepth = depthStack.pop();
+      max = Integer.max(max, tempDepth);
+      if (tempNode.left != null) {
+        treeStack.push(tempNode.left);
+        depthStack.push(tempDepth + 1);
+      }
+      if (tempNode.right != null) {
+        treeStack.push(tempNode.right);
+        depthStack.push(tempDepth + 1);
+      }
+    }
+    return max;
   }
 
   public int maxDepthIterativeBFS(TreeNode root) {
--- test/leetCode/LeetCode104Test.java
+++ test/leetCode/LeetCode104Test.java
@@ -47,4 +47,25 @@ public class LeetCode104Test {
                 new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, null, null, 10, 11, null, null})),
         4);
   }
+  @Test
+  public void maxDepthIterativeDFS() {
+    LeetCode104 l = new LeetCode104();
+    Assert.assertEquals(l.maxDepthIterativeDFS(TreeUtils.createTree(new Integer[] {})), 0);
+    Assert.assertEquals(l.maxDepthIterativeDFS(TreeUtils.createTree(new Integer[] {1})), 1);
+    Assert.assertEquals(
+        l.maxDepthIterativeDFS(TreeUtils.createTree(new Integer[] {1, 2, null})), 2);
+    Assert.assertEquals(
+        l.maxDepthIterativeDFS(TreeUtils.createTree(new Integer[] {1, 2, null, null, 3, 4})), 4);
+    Assert.assertEquals(
+        l.maxDepthIterativeDFS(
+            TreeUtils.createTree(
+                new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15})),
+        4);
+    Assert.assertEquals(
+        l.maxDepthIterativeDFS(
+            TreeUtils.createTree(
+                new Integer[] {1, 2, 3, 4, 5, 6, 7, 8, 9, null, null, 10, 11, null, null})),
+        4);
+  }
+  
 }
