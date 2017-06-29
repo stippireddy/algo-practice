1ed68b5ba931df50183dea2a29d729cbf5a01f5e
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-06-29 20:18:22 +0530

    http://www.geeksforgeeks.org/find-pythagorean-triplet-in-an-unsorted-array/

---
+++ src/geeksForGeeks/arrays/Arrays132.java
@@ -0,0 +1,37 @@
+package geeksForGeeks.arrays;
+
+import java.util.Arrays;
+
+/**
+ * @author Sarveswara Tippireddy
+ *     <p>This GeeksforGeeks problem can be found @
+ *     http://www.geeksforgeeks.org/find-pythagorean-triplet-in-an-unsorted-array/
+ */
+public class Arrays132 {
+  public boolean findPythogoreanTriplets(int[] a) {
+    for (int i = 0; i < a.length; i++) {
+      a[i] = a[i] * a[i];
+    }
+    Arrays.sort(a);
+    for (int i = a.length - 1; i > 1; i--) {
+      if (helper(a, i - 1, a[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean helper(int[] a, int k, int x) {
+    int i = 0, j = k;
+    while (i < j) {
+      if (a[i] + a[j] > x) {
+        j--;
+      } else if (a[i] + a[j] < x) {
+        i++;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ test/geeksForGeeks/arrays/Arrays132Test.java
@@ -0,0 +1,19 @@
+package geeksForGeeks.arrays;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class Arrays132Test {
+
+  @Test
+  public void findPythogoreanTriplets() {
+    Arrays132 a = new Arrays132();
+    assertTrue(a.findPythogoreanTriplets(new int[] {3, 1, 4, 6, 5}));
+    assertTrue(a.findPythogoreanTriplets(new int[] {1, 2, 4, 8, 6, 10}));
+    assertTrue(a.findPythogoreanTriplets(new int[] {13, 1, 4, 61, 5, 12}));
+    assertTrue(a.findPythogoreanTriplets(new int[] {25, 20, 15, 13, 7, 91, 201}));
+    assertFalse(a.findPythogoreanTriplets(new int[] {10, 4, 6, 12, 5}));
+  }
+}
