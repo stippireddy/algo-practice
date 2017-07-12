667ab578900978850d4b8fa2c38bc234a5fb7a7b
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-07-12 11:30:42 +0530

    http://www.geeksforgeeks.org/minimum-insertions-to-form-a-palindrome-with-permutations-allowed/

---
+++ src/geeksForGeeks/strings/MinInsertionsToPalindrome.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Sarveswara Tippireddy on Jul 12, 2017
+ *
+ * <p>This GeeksForGeeks problem can be found @
+ * http://www.geeksforgeeks.org/minimum-insertions-to-form-a-palindrome-with-permutations-allowed/
+ */
+package geeksForGeeks.strings;
+
+import java.util.Scanner;
+
+public class MinInsertionsToPalindrome {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    while (n-- > 0) {
+      System.out.println(getMinInsertions(sc.next()));
+    }
+    sc.close();
+  }
+
+  private static int getMinInsertions(String next) {
+    int[] count = new int[26];
+    for (char c : next.toCharArray()) {
+      count[c - 'a']++;
+    }
+    int oddCount = 0;
+    for (int i : count) {
+      if ((i & 1) != 0) {
+        oddCount++;
+      }
+    }
+    return oddCount > 0 ? oddCount - 1 : 0;
+  }
+}
