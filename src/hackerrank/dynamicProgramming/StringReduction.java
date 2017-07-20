0588e659cab4f23a8be59c7c7ec96cc8f7280646
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-07-20 15:24:22 +0530

    https://www.hackerrank.com/challenges/string-reduction

---
+++ src/hackerrank/dynamicProgramming/StringReduction.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Sarveswara Tippireddy on Jul 17, 2017
+ *
+ * <p>This GeeksForGeeks problem can be found @
+ * https://www.hackerrank.com/challenges/string-reduction
+ */
+package hackerrank.dynamicProgramming;
+
+import java.util.Scanner;
+
+public class StringReduction {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    while (n-- > 0) {
+      String input = sc.next();
+      System.out.println(reducedStringLength(input));
+    }
+    sc.close();
+  }
+
+  public static int reducedStringLength(String input) {
+    //TODO complete this
+    return 0;
+  }
+}
