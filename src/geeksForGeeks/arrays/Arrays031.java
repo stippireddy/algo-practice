73e67f08f5865e65cc960a05597b453dcb0d5eb3
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-07-21 21:38:44 +0530

    http://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/

--- src/geeksForGeeks/arrays/Arrays031.java
+++ src/geeksForGeeks/arrays/Arrays031.java
@@ -9,18 +9,22 @@ public class Arrays031 {
   public void sort(int[] input) {
     int low = 0, mid = 0, high = input.length - 1;
     while (mid <= high) {
-      if (input[mid] == 1) {
-        mid++;
-      } else if (input[mid] == 0) {
-        input[mid] = 1;
-        input[low] = 0;
+      if (input[mid] == 0) {
+        swap(input, low, mid);
         low++;
         mid++;
-      } else {
-        input[mid] = input[high];
-        input[high] = 2;
+      } else if (input[mid] == 2) {
+        swap(input, mid, high);
         high--;
+      } else {
+        mid++;
       }
     }
+  }
+
+  private void swap(int[] input, int a, int b) {
+    int temp = input[a];
+    input[a] = input[b];
+    input[b] = temp;
   }
 }
