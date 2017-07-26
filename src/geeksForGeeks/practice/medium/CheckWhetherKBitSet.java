b6364d96a51b76166184908e37f06837975318f5
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-07-26 23:53:29 +0530

    http://practice.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not/0

---
+++ src/geeksForGeeks/practice/medium/CheckWhetherKBitSet.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Sarveswara Tippireddy on Jul 22, 2017
+ *
+ * <p>This is a GeeksForGeeks problem. The problem statement can be found @
+ * http://practice.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not/0
+ *
+ * <p>The solution editorial can be found @
+ * http://www.geeksforgeeks.org/check-whether-k-th-bit-set-not/
+ */
+package geeksForGeeks.practice.medium;
+
+import java.util.Scanner;
+
+public class CheckWhetherKBitSet {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int numberOfTestCases = sc.nextInt();
+    while (numberOfTestCases-- > 0) {
+      int n = sc.nextInt();
+      int k = sc.nextInt();
+      if ((n & 1 << k) == 0) {
+        System.out.println("No");
+      } else {
+        System.out.println("Yes");
+      }
+    }
+    sc.close();
+  }
+}
