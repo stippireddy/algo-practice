e8bbd028475616ad011c4a3ed14476f6a63c4997
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-07-27 10:50:56 +0530

    http://practice.geeksforgeeks.org/problems/first-come-first-serve/0

---
+++ src/geeksForGeeks/practice/basic/FirstComeFirstServe.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Sarveswara Tippireddy on Jul 27, 2017
+ *
+ * <p>This is a GeeksForGeeks problem. The problem statement can be found @
+ * http://practice.geeksforgeeks.org/problems/first-come-first-serve/0
+ *
+ * <p>The solution editorial can be found @
+ * http://www.geeksforgeeks.org/first-element-occurring-k-times-array/
+ */
+package geeksForGeeks.practice.basic;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class FirstComeFirstServe {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    while (n-- > 0) {
+      int k = sc.nextInt();
+      int count = sc.nextInt();
+      int[] input = new int[k];
+      for (int i = 0; i < k; i++) {
+        input[i] = sc.nextInt();
+      }
+      System.out.println(getNum(input, count));
+    }
+    sc.close();
+  }
+
+  private static int getNum(int[] input, int count) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i : input) {
+      map.put(i, map.getOrDefault(i, 0) + 1);
+    }
+    for (int i : input) {
+      if (map.get(i) == count) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
