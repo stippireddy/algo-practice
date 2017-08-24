1f94e343fae9dd73393397258c95222b9e4c266c
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-08-24 11:22:48 -0400

    https://leetcode.com/problems/roman-to-integer/description/

---
+++ src/leetCode/LeetCode013.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Sarveswara Tippireddy on Aug 23, 2017
+ *
+ * <p>This LeetCode problem can be found @
+ * https://leetcode.com/problems/roman-to-integer/description/
+ */
+package leetCode;
+
+import java.util.HashMap;
+
+public class LeetCode013 {
+  
+  private static final HashMap<Character, Integer> keyMap = new HashMap<>();
+
+  static {
+    keyMap.put('M', 1000);
+    keyMap.put('D', 500);
+    keyMap.put('C', 100);
+    keyMap.put('L', 50);
+    keyMap.put('X', 10);
+    keyMap.put('V', 5);
+    keyMap.put('I', 1);
+  }
+
+  public int romanToInt(String s) {
+    int result = 0;
+    int prev = keyMap.get(s.charAt(0));
+    for (int i = 1; i < s.length(); i++) {
+      int curr = keyMap.get(s.charAt(i));
+      if (prev < curr) {
+        result -= prev;
+      } else {
+        result += prev;
+      }
+      prev = curr;
+    }
+    result += prev;
+    return result;
+  }
+}
---
+++ test/leetCode/LeetCode013Test.java
@@ -0,0 +1,16 @@
+package leetCode;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class LeetCode013Test {
+
+  @Test
+  public void romanToInt() {
+    LeetCode013 l = new LeetCode013();
+    assertEquals(l.romanToInt("MMMCMXCIX"), 3999);
+    assertEquals(l.romanToInt("MMMCMXLIX"), 3949);
+    assertEquals(l.romanToInt("DCXXI"), 621);
+  }
+}
