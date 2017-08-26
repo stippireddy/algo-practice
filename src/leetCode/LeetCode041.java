2cec4e9aee7c835f1d68d3d1cdf9ab8040dc01d9
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-08-26 18:17:01 -0400

    https://leetcode.com/problems/first-missing-positive/description/

---
+++ src/leetCode/LeetCode041.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Sarveswara Tippireddy on Aug 26, 2017
+ *
+ * <p>
+ * This LeetCode problem can be found @
+ * https://leetcode.com/problems/first-missing-positive/description/
+ */
+package leetCode;
+
+public class LeetCode041 {
+  public int firstMissingPositive(int[] nums) {
+    for (int i = 0; i < nums.length;) {
+      if (nums[i] > 0 && nums[i] < nums.length + 1) {
+        // The following condition takes care of the possibility
+        // of infinite loop if the numbers being swapped are the same
+        if (nums[i] != nums[nums[i] - 1]) {
+          int temp = nums[nums[i] - 1];
+          nums[nums[i] - 1] = nums[i];
+          nums[i] = temp;
+          continue;
+        }
+      }
+      i++;
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+    return nums.length + 1;
+  }
+}
---
+++ test/leetCode/LeetCode041Test.java
@@ -0,0 +1,19 @@
+package leetCode;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class LeetCode041Test {
+
+  @Test
+  public void firstMissingPositive() {
+    LeetCode041 l = new LeetCode041();
+    assertEquals(l.firstMissingPositive(new int[] {}), 1);
+    assertEquals(l.firstMissingPositive(new int[] {3, 4, -1, 1}), 2);
+    assertEquals(l.firstMissingPositive(new int[] {1, 2, 0}), 3);
+    assertEquals(l.firstMissingPositive(new int[] {2, 1, 3}), 4);
+    assertEquals(l.firstMissingPositive(new int[] {10, 11, 120}), 1);
+    assertEquals(l.firstMissingPositive(new int[] {1, 1}), 2);
+  }
+}
