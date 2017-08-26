56b1b9607c67dd0c1f5c4e95616dce824bf82354
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-08-26 12:25:25 -0400

    https://leetcode.com/problems/find-the-duplicate-number/description/

--- src/leetCode/LeetCode287.java
+++ src/leetCode/LeetCode287.java
@@ -1,12 +1,19 @@
+/**
+ * Created by Sarveswara Tippireddy
+ *
+ * <p>
+ * This LeetCode problem can be found @
+ * https://leetcode.com/problems/find-the-duplicate-number/description/
+ */
 package leetCode;
 
 import java.util.HashMap;
 
 public class LeetCode287 {
-  
+
   public int findDuplicateUsingHashMap(int[] nums) {
     HashMap<Integer, Integer> map = new HashMap<>();
-    for(int i : nums){
+    for (int i : nums) {
       int count = map.getOrDefault(i, 0);
       if (count == 1) {
         return i;
@@ -16,7 +23,7 @@ public class LeetCode287 {
     }
     return -1;
   }
-  
+
   public int findDuplicateBruteForce(int[] nums) {
     for (int i = 0; i < nums.length - 1; i++) {
       for (int j = i + 1; j < nums.length; j++) {
@@ -27,6 +34,38 @@ public class LeetCode287 {
     }
     return -1;
   }
-  
-  public int findDuplicateEfficient(int[] nums) {}
+
+  public int findDuplicateUsingBinarySearch(int[] nums) {
+    int low = 1, high = nums.length - 1;
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      int count = 0;
+      for (int i : nums) {
+        if (i <= mid) {
+          count++;
+        }
+      }
+      if (count > mid) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return low;
+  }
+
+  public int findDuplicateUsingCycleDetection(int[] nums) {
+    int tortoise = nums[0];
+    int hare = nums[nums[0]];
+    while (hare != tortoise) {
+      tortoise = nums[tortoise];
+      hare = nums[nums[hare]];
+    }
+    hare = 0;
+    while (hare != tortoise) {
+      hare = nums[hare];
+      tortoise = nums[tortoise];
+    }
+    return tortoise;
+  }
 }
--- test/leetCode/LeetCode287Test.java
+++ test/leetCode/LeetCode287Test.java
@@ -10,5 +10,36 @@ public class LeetCode287Test {
   public void findDuplicateUsingHashMap() {
     LeetCode287 l = new LeetCode287();
     assertEquals(l.findDuplicateUsingHashMap(new int[] {1, 1}), 1);
+    assertEquals(l.findDuplicateUsingHashMap(new int[] {1, 2, 3, 1, 4, 5}), 1);
+    assertEquals(l.findDuplicateUsingHashMap(new int[] {10, 8, 7, 9, 4, 7, 7, 2, 7, 1, 3}), 7);
+    assertEquals(l.findDuplicateUsingHashMap(new int[] {1, 3, 4, 2, 2}), 2);
+  }
+
+  @Test
+  public void findDuplicateBruteForce() {
+    LeetCode287 l = new LeetCode287();
+    assertEquals(l.findDuplicateBruteForce(new int[] {1, 1}), 1);
+    assertEquals(l.findDuplicateBruteForce(new int[] {1, 2, 3, 1, 4, 5}), 1);
+    assertEquals(l.findDuplicateBruteForce(new int[] {10, 8, 7, 9, 4, 7, 7, 2, 7, 1, 3}), 7);
+    assertEquals(l.findDuplicateBruteForce(new int[] {1, 3, 4, 2, 2}), 2);
+  }
+
+  @Test
+  public void findDuplicateUsingBinarySearch() {
+    LeetCode287 l = new LeetCode287();
+    assertEquals(l.findDuplicateUsingBinarySearch(new int[] {1, 1}), 1);
+    assertEquals(l.findDuplicateUsingBinarySearch(new int[] {1, 2, 3, 1, 4, 5}), 1);
+    assertEquals(l.findDuplicateUsingBinarySearch(new int[] {10, 8, 7, 9, 4, 7, 7, 2, 7, 1, 3}), 7);
+    assertEquals(l.findDuplicateUsingBinarySearch(new int[] {1, 3, 4, 2, 2}), 2);
+  }
+
+  @Test
+  public void findDuplicateUsingCycleDetection() {
+    LeetCode287 l = new LeetCode287();
+    assertEquals(l.findDuplicateUsingCycleDetection(new int[] {1, 1}), 1);
+    assertEquals(l.findDuplicateUsingCycleDetection(new int[] {1, 2, 3, 1, 4, 5}), 1);
+    assertEquals(l.findDuplicateUsingCycleDetection(new int[] {10, 8, 7, 9, 4, 7, 7, 2, 7, 1, 3}),
+        7);
+    assertEquals(l.findDuplicateUsingCycleDetection(new int[] {1, 3, 4, 2, 2}), 2);
   }
 }
