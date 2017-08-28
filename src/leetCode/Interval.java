fdef7892d7993c965cff4168923ee9b1f71c5e10
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-08-28 17:57:32 -0400

    https://leetcode.com/problems/merge-intervals/description/

---
+++ src/leetCode/Interval.java
@@ -0,0 +1,25 @@
+package leetCode;
+
+public class Interval {
+  int start;
+  int end;
+
+  public Interval() {
+    start = 0;
+    end = 0;
+  }
+
+  public Interval(int s, int e) {
+    start = s;
+    end = e;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj.getClass() != Interval.class) {
+      return false;
+    }
+    Interval i = (Interval) obj;
+    return this.start == i.start && this.end == i.end;
+  }
+}
---
+++ src/leetCode/LeetCode435.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Sarveswara Tippireddy on Aug 25, 2017
+ *
+ * <p>
+ * This LeetCode problem can be found @
+ * https://leetcode.com/problems/non-overlapping-intervals/description/
+ */
+package leetCode;
+
+public class LeetCode435 {
+  public int eraseOverlapIntervals(Interval[] intervals) {
+    // TODO
+  }
+}
