78f0e7c5cae3051744fee4b06348bebdd47f7d7a
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-08-27 21:16:47 -0400

    LeetCode628 Maximum Product of Three Numbers

--- src/leetCode/LeetCode628.java
+++ src/leetCode/LeetCode628.java
@@ -3,9 +3,9 @@ package leetCode;
 import java.util.Arrays;
 
 public class LeetCode628 {
-  public static int maximumProduct(int[] nums) {
+  public int maximumProduct(int[] nums) {
     Arrays.sort(nums);
-    int l = nums.length;
-    return nums[l - 1] * Integer.max(nums[l - 2] * nums[l - 3], nums[0] * nums[1]);
+    int n = nums.length;
+    return Integer.max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
   }
 }
---
+++ test/leetCode/LeetCode628Test.java
@@ -0,0 +1,20 @@
+package leetCode;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class LeetCode628Test {
+
+  @Test
+  public void maximumProduct() {
+    LeetCode628 l = new LeetCode628();
+    assertEquals(l.maximumProduct(new int[] {1, 2, 3}), 6);
+    assertEquals(l.maximumProduct(new int[] {1, 2, 3, 4, 5, 6}), 120);
+    assertEquals(l.maximumProduct(new int[] {1, 2, 3, -4, -5, 6}), 120);
+    assertEquals(l.maximumProduct(new int[] {1, 2, -3, 4, -5, 6}), 90);
+    assertEquals(l.maximumProduct(new int[] {-1, 2, 3, 4, 5, -6}), 60);
+    assertEquals(l.maximumProduct(new int[] {-1, -2, -3, -4, -5, -6}), -6);
+    assertEquals(l.maximumProduct(new int[] {-1, -2, -3, 0, 5, 6}), 36);
+  }
+}
