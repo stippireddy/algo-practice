0d1febda44460a63257181cf3ae50c3395589d35
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-08-31 11:30:04 -0400

    https://leetcode.com/problems/number-of-1-bits/description/

--- src/leetCode/LeetCode191.java
+++ src/leetCode/LeetCode191.java
@@ -4,21 +4,43 @@
  * <p>
  * This LeetCode problem can be found @ https://leetcode.com/problems/number-of-1-bits/description/
  * 
- * This solution copies the idea from this source
- * (https://discuss.leetcode.com/topic/11385/simple-java-solution-bit-shifting). Do read it for
- * better understanding.
+ * Sources for the ideas used in the solutions: 
+ * https://leetcode.com/articles/number-1-bits/
+ * https://discuss.leetcode.com/topic/11385/simple-java-solution-bit-shifting. 
+ * Do read them for better understanding.
  */
 package leetCode;
 
 public class LeetCode191 {
   // The question requires n to be treated as an unsigned integer
-  public int hammingWeight(int n) {
+  public int hammingWeightUsingUnsignedShift(int n) {
     int count = 0;
     // Note the use of n!=0 and not n>0. Why?
     while (n != 0) {
       count += (n & 1);
       // Note this use of unsigned bit shift. Why?
       n = n >>> 1;
+    }
+    return count;
+  }
+
+  public int hammingWeightUsingShiftingMask(int n) {
+    int count = 0;
+    // Note the use of n!=0 and not n>0. Why?
+    int mask = 1;
+    for (int i = 0; i < 32; i++) {
+      count += (n & mask) == 0 ? 0 : 1;
+      mask = mask << 1;
+    }
+    return count;
+  }
+
+  public int hammingWeightByCountingLSB(int n) {
+    int count = 0;
+    // Note the use of n!=0 and not n>0. Why?
+    while (n != 0) {
+      count++;
+      n &= (n - 1);
     }
     return count;
   }
--- test/leetCode/LeetCode191Test.java
+++ test/leetCode/LeetCode191Test.java
@@ -6,12 +6,42 @@ import org.testng.annotations.Test;
 public class LeetCode191Test {
 
   @Test
-  public void hammingWeight() {
+  public void hammingWeightUsingUnsignedShift() {
     LeetCode191 l = new LeetCode191();
-    Assert.assertEquals(l.hammingWeight(Integer.MAX_VALUE + 1), 1);
-    Assert.assertEquals(l.hammingWeight(Integer.MAX_VALUE + Integer.MAX_VALUE), 31);
-    Assert.assertEquals(l.hammingWeight(Integer.MAX_VALUE + 2), 2);
-    Assert.assertEquals(l.hammingWeight(4096), 1);
-    Assert.assertEquals(l.hammingWeight(4095), 12);
+    Assert.assertEquals(l.hammingWeightUsingUnsignedShift(Integer.MAX_VALUE + 1), 1);
+    Assert.assertEquals(l.hammingWeightUsingUnsignedShift(Integer.MAX_VALUE + Integer.MAX_VALUE),
+        31);
+    Assert.assertEquals(l.hammingWeightUsingUnsignedShift(Integer.MAX_VALUE + 2), 2);
+    Assert.assertEquals(l.hammingWeightUsingUnsignedShift(4096), 1);
+    Assert.assertEquals(l.hammingWeightUsingUnsignedShift(4095), 12);
+  }
+
+  @Test
+  public void hammingWeightUsingShiftingMask() {
+    LeetCode191 l = new LeetCode191();
+    Assert.assertEquals(l.hammingWeightUsingShiftingMask(Integer.MAX_VALUE + 1), 1);
+    Assert.assertEquals(l.hammingWeightUsingShiftingMask(Integer.MAX_VALUE + Integer.MAX_VALUE),
+        31);
+    Assert.assertEquals(l.hammingWeightUsingShiftingMask(Integer.MAX_VALUE + 2), 2);
+    Assert.assertEquals(l.hammingWeightUsingShiftingMask(4096), 1);
+    Assert.assertEquals(l.hammingWeightUsingShiftingMask(4095), 12);
+  }
+
+  @Test
+  public void hammingWeightByCountingLSB() {
+    LeetCode191 l = new LeetCode191();
+    Assert.assertEquals(l.hammingWeightByCountingLSB(Integer.MAX_VALUE + 1), 1);
+    Assert.assertEquals(l.hammingWeightByCountingLSB(Integer.MAX_VALUE + Integer.MAX_VALUE), 31);
+    Assert.assertEquals(l.hammingWeightByCountingLSB(Integer.MAX_VALUE + 2), 2);
+    Assert.assertEquals(l.hammingWeightByCountingLSB(4096), 1);
+    Assert.assertEquals(l.hammingWeightByCountingLSB(4095), 12);
+  }
+
+  @Test
+  public void test() {
+    System.out.println(Integer.toBinaryString(10));
+    System.out.println(Integer.toBinaryString(~(9)));
+    System.out.println(Integer.toBinaryString(10 & 10 - 1));
+    System.out.println(Integer.toBinaryString(10 & ~(10 - 1)));
   }
 }
