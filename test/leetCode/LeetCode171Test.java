b50a622a541c853ec007ee750365f48f02dc46c6
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-08-30 22:49:38 -0400

    https://leetcode.com/problems/excel-sheet-column-number/description/

---
+++ test/leetCode/LeetCode171Test.java
@@ -0,0 +1,22 @@
+package leetCode;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class LeetCode171Test {
+
+  @Test
+  public void titleToNumber() {
+    LeetCode171 l = new LeetCode171();
+    assertEquals(l.titleToNumber("A"), 1);
+    assertEquals(l.titleToNumber("B"), 2);
+    assertEquals(l.titleToNumber("Z"), 26);
+    assertEquals(l.titleToNumber("AA"), 27);
+    assertEquals(l.titleToNumber("AB"), 28);
+    assertEquals(l.titleToNumber("AZ"), 52);
+    assertEquals(l.titleToNumber("BA"), 53);
+    assertEquals(l.titleToNumber("AAA"), 703);
+    assertEquals(l.titleToNumber("AAAA"), 18279);
+  }
+}
