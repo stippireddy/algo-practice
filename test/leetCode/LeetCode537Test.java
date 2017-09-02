b0ae0e3efc25e176b79160b836e54426fb6cad53
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-01 23:45:00 -0400

    https://leetcode.com/problems/complex-number-multiplication/description/

---
+++ test/leetCode/LeetCode537Test.java
@@ -0,0 +1,14 @@
+package leetCode;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class LeetCode537Test {
+
+  @Test
+  public void complexNumberMultiply() {
+    LeetCode537 l = new LeetCode537();
+    assertEquals(l.complexNumberMultiply("1+1i", "1+1i"), "0+2i");
+  }
+}
