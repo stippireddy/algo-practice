4976537924df4d3a9b6f8be903f9534b62849770
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-02 10:02:08 -0400

    https://leetcode.com/problems/perfect-number/description/

---
+++ src/leetCode/LeetCode507.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Sarveswara Tippireddy on Sep 2, 2017
+ *
+ * <p>
+ * This LeetCode problem can be found @ https://leetcode.com/problems/perfect-number/description/
+ * 
+ */
+package leetCode;
+
+public class LeetCode507 {
+  public boolean checkPerfectNumber(int num) {
+    if (num == 1) {
+      return false;
+    }
+    int sum = 1;
+    // The use of < Math.sqrt is because Perfect Squares can't be Perfect Numbers
+    // Also note that negative number case is handled by Math.sqrt
+    for (int i = 2; i < Math.sqrt(num); i++) {
+      if ((num % i) == 0) {
+        sum += i;
+        sum += num / i;
+      }
+    }
+    return sum == num;
+  }
+}
