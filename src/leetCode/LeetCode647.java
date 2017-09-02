2d5b305ee9f5279f92c33533b86e858c2d86c45d
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-02 14:41:38 -0400

    https://leetcode.com/problems/palindromic-substrings/description/

---
+++ src/leetCode/LeetCode647.java
@@ -0,0 +1,71 @@
+/**
+ * Created by Sarveswara Tippireddy on Sep 2, 2017
+ *
+ * <p>
+ * This LeetCode problem can be found @
+ * https://leetcode.com/problems/palindromic-substrings/description/
+ * 
+ */
+package leetCode;
+
+public class LeetCode647 {
+  public int countSubstringsBruteForce(String s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      for (int j = i; j < s.length(); j++) {
+        if (isPalindrome(s, i, j)) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  private boolean isPalindrome(String s, int i, int j) {
+    if (i == j) {
+      return true;
+    }
+    while (i < j) {
+      if (s.charAt(i) != s.charAt(j)) {
+        return false;
+      } else {
+        i++;
+        j--;
+      }
+    }
+    return true;
+  }
+
+  public int countSubstringsEfficient(String s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      int left = i - 1, right = i + 1;
+      count++;
+      while (left >= 0 && right < s.length()) {
+        if (s.charAt(left) == s.charAt(right)) {
+          left--;
+          right++;
+          count++;
+        } else {
+          break;
+        }
+      }
+    }
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) == s.charAt(i - 1)) {
+        int left = i - 2, right = i + 1;
+        count++;
+        while (left >= 0 && right < s.length()) {
+          if (s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+            count++;
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    return count;
+  }
+}
---
+++ test/leetCode/LeetCode647Test.java
@@ -0,0 +1,24 @@
+package leetCode;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class LeetCode647Test {
+
+  @Test
+  public void countSubstringsBruteForce() {
+    LeetCode647 l = new LeetCode647();
+    assertEquals(l.countSubstringsBruteForce("abc"), 3);
+    assertEquals(l.countSubstringsBruteForce("aaa"), 6);
+    assertEquals(l.countSubstringsEfficient("aaaaa"), 15);
+  }
+
+  @Test
+  public void countSubstringsEfficient() {
+    LeetCode647 l = new LeetCode647();
+    assertEquals(l.countSubstringsEfficient("abc"), 3);
+    assertEquals(l.countSubstringsEfficient("aaa"), 6);
+    assertEquals(l.countSubstringsEfficient("aaaaa"), 15);
+  }
+}
