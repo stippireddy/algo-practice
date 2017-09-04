56dd957d73f8a73ddcdf347f0fc7e8ace4595b39
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-04 14:39:44 -0400

    https://leetcode.com/problems/power-of-three/

---
+++ src/leetCode/LeetCode326.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Sarveswara Tippireddy on Aug 30, 2017
+ *
+ * <p>
+ * This LeetCode problem can be found @ https://leetcode.com/problems/power-of-three/description/
+ */
+package leetCode;
+
+public class LeetCode326 {
+  // Find the maximum possible power of 3 within the range of java integer.
+  // If the max is perfectly divisible by the given n, then n is a power of 3.
+  public boolean isPowerOfThree(int n) {
+    if (n < 1) {
+      return false;
+    }
+    int maxPowerOf3 =
+        (int) Math.pow(3, (int) (Math.log10((double) Integer.MAX_VALUE) / Math.log10(3)));
+    return maxPowerOf3 % n == 0;
+  }
+}
---
+++ test/leetCode/LeetCode326Test.java
@@ -0,0 +1,27 @@
+package leetCode;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class LeetCode326Test {
+
+  @Test
+  public void isPowerOfThree() {
+    LeetCode326 l = new LeetCode326();
+    assertTrue(l.isPowerOfThree(1));
+    assertTrue(l.isPowerOfThree(3));
+    assertTrue(l.isPowerOfThree(9));
+    assertTrue(l.isPowerOfThree(27));
+    assertTrue(l.isPowerOfThree(81));
+    assertTrue(l.isPowerOfThree(243));
+    assertFalse(l.isPowerOfThree(2));
+    assertFalse(l.isPowerOfThree(8));
+    assertFalse(l.isPowerOfThree(25));
+    assertFalse(l.isPowerOfThree(84));
+    assertFalse(l.isPowerOfThree(241));
+    assertFalse(l.isPowerOfThree(0));
+    assertFalse(l.isPowerOfThree(-9));
+  }
+}
