42db9dc3bb1032a2c21866005eb590e6413d5d85
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-04 14:16:06 -0400

    https://leetcode.com/problems/intersection-of-two-linked-lists/

--- src/leetCode/LeetCode160.java
+++ src/leetCode/LeetCode160.java
@@ -1,40 +1,39 @@
+/**
+ * Created by Sarveswara Tippireddy on Sep 1, 2017
+ *
+ * <p>
+ * This LeetCode problem can be found @
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ * 
+ */
 package leetCode;
 
 public class LeetCode160 {
   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-    ListNode temp1 = headA;
-    int length1 = 0;
-    while (temp1 != null) {
-      temp1 = temp1.next;
-      length1++;
+    int length1 = getLength(headA);
+    int length2 = getLength(headB);
+    while (length1 > length2) {
+      headA = headA.next;
+      length1--;
     }
-    temp1 = headB;
-    int length2 = 0;
-    while (temp1 != null) {
-      temp1 = temp1.next;
-      length2++;
+    while (length2 > length1) {
+      headB = headB.next;
+      length2--;
     }
-    
-    if (length1 > length2) {
-      int diff = length1 - length2;
-      while (diff > 0) {
-        headA = headA.next;
-        diff--;
-      }
-    } else if (length2 > length1) {
-      int diff = length2 - length1;
-      while (diff > 0) {
-        headB = headB.next;
-        diff--;
-      }
-    }
-    while (headA != null && headB != null) {
-      if (headA.val == headB.val) {
-        return headA;
-      }
+    while ((headA != null && headB != null) && (headA.val != headB.val)) {
       headA = headA.next;
       headB = headB.next;
     }
-    return null;
+    return headA;
+  }
+
+  private int getLength(ListNode root) {
+    int length = 0;
+    while (root != null) {
+      root = root.next;
+      length++;
+    }
+    return length;
+
   }
 }
--- test/leetCode/LeetCode160Test.java
+++ test/leetCode/LeetCode160Test.java
@@ -1,6 +1,8 @@
 package leetCode;
 
-import org.testng.Assert;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test;
 
 public class LeetCode160Test {
@@ -8,18 +10,11 @@ public class LeetCode160Test {
   @Test
   public void getIntersectionNode() {
     LeetCode160 l = new LeetCode160();
-    Assert.assertEquals(
-        l.getIntersectionNode(
-                ListNode.createList(new int[] {1, 2, 3, 4, 5, 6}),
-                ListNode.createList(new int[] {9, 10, 5, 6}))
-            .val,
-        5);
-    Assert.assertNull(
-        l.getIntersectionNode(ListNode.createList(new int[] {1, 2, 3, 4, 5, 6}), null));
-    Assert.assertNull(
-        l.getIntersectionNode(
-            ListNode.createList(new int[] {1, 2, 3, 4, 5, 6}),
-            ListNode.createList(new int[] {7, 8, 9, 10, 11, 12, 13})));
-    Assert.assertNull(null, null);
+    assertTrue(l.getIntersectionNode(ListUtils.createList(new int[] {1, 2, 3, 4, 5, 6}),
+        ListUtils.createList(new int[] {9, 10, 5, 6})).val == 5);
+    assertNull(l.getIntersectionNode(ListUtils.createList(new int[] {1, 2, 3, 4, 5, 6}), null));
+    assertNull(l.getIntersectionNode(ListUtils.createList(new int[] {1, 2, 3, 4, 5, 6}),
+        ListUtils.createList(new int[] {7, 8, 9, 10, 11, 12, 13})));
+    assertNull(null, null);
   }
 }
