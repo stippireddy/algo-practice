662509faaa262fe224a0d7397c250e6cc0454331
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-06 09:10:41 -0400

    https://leetcode.com/problems/longest-substring-without-repeating-characters/description/

--- src/leetCode/LeetCode003.java
+++ src/leetCode/LeetCode003.java
@@ -11,17 +11,17 @@ package leetCode;
 public class LeetCode003 {
   public int lengthOfLongestSubstring(String s) {
     char[] sArray = s.toCharArray();
-    int[] freq = new int[26];
+    int[] freq = new int[128];
     int i = 0, j = 0, maxLength = 0;
     while (j < sArray.length) {
-      if (i < j && freq[sArray[i] - 'a'] > 0) {
-        freq[sArray[i] - 'a'] = 0;
-        i++;
-      } else if (freq[sArray[j] - 'a'] == 0) {
+      if (freq[sArray[j] - 'a'] == 0) {
         freq[sArray[j] - 'a'] = 1;
         maxLength = Integer.max(maxLength, j - i + 1);
         j++;
-      }
+      } else {
+        freq[sArray[i] - 'a'] = 0;
+        i++;
+      } 
     }
     return maxLength;
   }
