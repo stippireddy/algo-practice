d5f793bafc82e3302c280e95cd1eb601cfa37800
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-13 23:59:35 -0400

    https://leetcode.com/problems/unique-paths/description/

--- test/leetCode/LeetCode062Test.java
+++ test/leetCode/LeetCode062Test.java
@@ -9,7 +9,7 @@ public class LeetCode062Test {
   @Test
   public void uniquePaths() {
     LeetCode062 l = new LeetCode062();
-    // assertEquals(l.uniquePaths(1, 2), 1);
+    assertEquals(l.uniquePaths(1, 2), 1);
     assertEquals(l.uniquePaths(10, 10), 48620);
     assertEquals(l.uniquePaths(20, 20), 985525432);
     assertEquals(l.uniquePaths(24, 23), 2136694232);
