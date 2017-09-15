cc5205850fb11edad4747ab5697f3f4328ff8f41
Sarveswara Tippireddy <stippireddy@users.noreply.github.com>
2017-09-14 22:08:41 -0400

    https://leetcode.com/problems/contains-duplicate/description/

--- src/leetCode/LeetCode217.java
+++ src/leetCode/LeetCode217.java
@@ -1,12 +1,29 @@
+/**
+ * <p>
+ * This LeetCode problem can be found @
+ * https://leetcode.com/problems/contains-duplicate/description/
+ * 
+ */
 package leetCode;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class LeetCode217 {
-  public boolean containsDuplicate(int[] nums) {
+  public boolean containsDuplicateUsingSort(int[] nums) {
     Arrays.sort(nums);
     for (int i = 0; i < nums.length - 1; i++) {
       if (nums[i] == nums[i + 1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean containsDuplicateUsingHashing(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+    for (int i : nums) {
+      if (!set.add(i)) {
         return true;
       }
     }
